75907cb849302a2a6a5cc0faa2d3bd13f4264afc
Victor Zhang <victor.g.zhang@gmail.com>
2018-08-01 15:34:30 +0900

    added BEEP and self-test commands; not tested

--- socketLS372.py
+++ socketLS372.py
@@ -10,7 +10,7 @@ local_hostname = socket.gethostname()
 
 localfqdn = socket.getfqdn()
 
-# This is the IP address of the Lake Shore Model 372 device
+# This is the IP address of the Lake Shore Model 372 device (now referred to as LS372)
 ip_address = "133.11.164.138" #socket.gethostbyname("")
 
 # The Lake Shore Model 372 device connects to port 7777 (see manual)
@@ -24,15 +24,28 @@ print("Connecting to %s (%s) at %s" % (local_hostname, localfqdn, ip_address))
 sock.send("*IDN?LF")
 while True:
     data = sock.recv(64) # Wait for up to 1 kbyte of data to be returned (if fewer bytes returned and the instrument signals message complete, the socket terminates and this data is returned)
+    print ("Data:")
     if data:
         # output received data
-        print ("Data: %s" % data)
+        print ("%s" % data)
     else:
         # no more data -- quit the loop
         print ("no more data.")
         print ("----------------\n")
         break
-
+sock.send("BEEP 1LF") # Should cause the LS372 to beep
+sock.send("*TST?LF") # Self-test: retunrs 0 if no errors, 1 if errors found
+while True:
+    data = sock.recv(64) # Wait for up to 1 kbyte of data to be returned (if fewer bytes returned and the instrument signals message complete, the socket terminates and this data is returned)
+    print ("Data:")
+    if data:
+        # output received data
+        print ("%s" % data)
+    else:
+        # no more data -- quit the loop
+        print ("no more data.")
+        print ("----------------\n")
+        break
 
 #randomData = [243,45242,32,43,5,2,"SDF","123W","313"]
 #for ele in randomData:
