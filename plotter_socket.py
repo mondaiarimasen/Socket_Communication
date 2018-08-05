46428dad1d9fc983c6fbfe7e6aa7f6f40a921bce
Victor Zhang <victor.g.zhang@gmail.com>
2018-08-06 08:22:48 +0900

    no extraneous lines, well commented, draws separate graph for each channel and one graph w/ all

--- plotter_socket.py
+++ plotter_socket.py
@@ -1,38 +1,38 @@
+# Victor Zhang, August 6, 2018
+# Plotter for Socket Data (Temperature)
+# Python
+
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
-from itertools import islice
 import pandas as pd
 
-allData = np.array(list(str(i) for i in range(0,9)))
-print("This is allData")
-print(allData)
-tempData = np.array(9)
+## Variables
+fileName = 'tempData.dat'
 color = ['b', 'g', 'r', 'c', 'm', 'y', 'k', '0.5'] # 0.5 is a gray color
-skip = 0 # how many rows to skip over
-test = np.array(9)
+step = 1 # step when reading array (lowest can be 1)
+channelNames = {1:'PT2 Head', 2:'PT2 Plate', 3:'1 K Plate', 4:'Still', 5:'mK Plate Cernox', 6:'PT1 Head', 7:'PT1 Plate', 8:'mK Plate RuOx'}
 
+## Plotting
+allData = pd.read_csv(fileName)
 
-allData=np.loadtxt(open('tempData.dat','r'),dtype='object',delimiter=',',)
-print("This is allData")
-#print(allData[1:2,2:3])
-#allData = np.asarray(allData)
-print(allData)
-print(allData[:,:1])
-
-allData = pd.read_csv('tempData.dat')
-print(allData['Time'].values)
-'''
-for i in range(0,9):
-	plt.plot(allData[:,:1][i][0], int(allData[:,i+1:i+2][]), colors[i])
-# beautify the x-labels
-plt.gcf().autofmt_xdate()
-
-plt.show()
-'''
+plt.figure(figsize=(12,8))
 for i in range(0,8):
-	plt.plot(allData['Time'].values,allData[str(i+1)].values.astype(np.float),color[i])
-plt.legend(('Ch. 1', 'Ch. 2', 'Ch. 3', 'Ch. 4', 'Ch. 5', 'Ch. 6', 'Ch. 7', 'Ch. 8'),
-           loc='upper right')
-plt.gcf().autofmt_xdate()
+	plt.plot(allData['Time'].values,allData[str(i+1)].values[::step].astype(np.float),color[i])
+plt.legend(list('Chl. ' + str(i+1) for i in range(0, 8)),loc='upper right')
+plt.gcf().autofmt_xdate() # beautify the x-labels
+plt.gcf().subplots_adjust(bottom=0.15)
+plt.title('Temperature of Channels vs. Time')
+plt.xlabel('Date and Time')
+plt.ylabel('Temperature (K)')
+
+for i in range(0,8):
+	plt.figure(figsize=(12,8))
+	plt.plot(allData['Time'].values,allData[str(i+1)].values[::step].astype(np.float),color[i])
+	plt.gcf().autofmt_xdate()
+	plt.gcf().subplots_adjust(bottom=0.25)
+	plt.title(channelNames[i+1] + ', (Channel ' + str(i+1) + '), Temperature vs. Time')
+	plt.xlabel('Date and Time')
+	plt.ylabel('Temperature (K)')
+
 plt.show()
