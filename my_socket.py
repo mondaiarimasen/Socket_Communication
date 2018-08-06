dd3b01c0e62bdef1c9ba6da5fde4979d2b0cdbb7
Victor Zhang <victor.g.zhang@gmail.com>
2018-08-06 10:55:58 +0900

    cleaned up version, with comments

--- my_socket.py
+++ my_socket.py
@@ -1,30 +1,25 @@
+# Victor Zhang, August 6, 2018
+# Temperature Acquisition from Lake Shore 372 device
+# Python
 import socket
 from datetime import datetime
 import time
-#import numpy as np
 
 ## Variables
 ip_address = "192.168.0.12"
 brght = 1 # 0=25%, 1=50%, 2=75%, 3=100%
-returnData = ""
 date_time = ""
 allTemp = ""
-count = 0
 sleepTime = 60 # how many seconds between temperature taking
-#allData = np.array(list(i for i in range(0,9)))
-#print("This is allData:")
-#print(allData)
-#tempData = np.zeros(9)
 
 ## Constants
 rdgst_dict = {"000":"Valid reading is present", "001":"CS OVL", "002":"VCM OVL", "004":"VMIX OVL", "008":"VDIF OVL", "016":"R. OVER", "032":"R. UNDER", "064":"T. OVER", "128":"T. UNDER"}
 term = "\r\n"
 
 ## Data file
-file = open("tempData.dat", "w")
+fileName = 'tempData.dat'
 
-
-## Starting socket communication and data acquisition
+## Starting socket communication
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 local_hostname = socket.gethostname()
 localfqdn = socket.getfqdn()
@@ -32,6 +27,7 @@ server_address = (ip_address, 7777)
 sock.connect(server_address)
 print("Connecting to %s at Port %s" % server_address)
 
+# Identification query
 sock.send("*IDN?" + term)
 data = sock.recv(1024)
 print("Identification: ")
@@ -41,6 +37,7 @@ else:
     print("no more data")
     print("-------------\n")
 
+# Network Configuration query
 sock.send("NETID?" + term)
 data = sock.recv(1024)
 print("Network Configuration: ")
@@ -50,6 +47,7 @@ else:
     print("no more data")
     print("-------------\n")
 
+# Brightness
 print("Changing brightness to %s%%" % str((brght+1)*25))
 sock.send("BRIGT " + str(brght) + term)
 sock.send("BRIGT?" + term)
@@ -60,6 +58,7 @@ else:
     print("no more data")
     print("-------------\n")
 
+# Self-Test query
 print("Checking for errors (0 for none, 1 for errors found):")
 sock.send("*TST?" + term)
 data = sock.recv(1024)
@@ -69,17 +68,15 @@ else:
     print("no more data")
     print("-------------\n")
 
-print("Time,1,2,3,4,5,6,7,8,")
-file.write("Time,1,2,3,4,5,6,7,8\n")
-
 ## Starting the data acquisition
+fileName = 'tempData.dat'
+print("Time,1,2,3,4,5,6,7,8,")
+file.write("Time,1,2,3,4,5,6,7,8,\n")
 
 while True:
     date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print date_time
     allTemp = date_time + ","
-    #tempData[0] = date_time
-
 
     print("Status and Reading of Thermometers:")
     for i in range(0,8):
@@ -88,35 +85,27 @@ while True:
         data = sock.recv(1024)[:-2]
         if data:
             pass
-        #    print("Channel %s: %s " % (str(i+1),rdgst_dict[data]))
-        #    print("(bit weighting: %s)" % data)
         else:
             print("Error at Channel %s (RDGST? command)" % str(i+1))
             print("-------------\n")
-        
+
         command2 = "RDGK? " + str(i+1) + term # interestingly, there is another Kelvin Reading Query: KRDG?, see manual
         sock.send(command2)
         data = sock.recv(1024)[:-2]
         allTemp += data +","
-        #tempData[i+1] = data
-        #print(float(data))
         if data:
             pass
-        #    print("Temperature at Channel %s: %s\n"% (str(i+1),data))
         else:
             print("Error at Channel %s (RDGK? command)" % str(i+1))
             print("-------------\n")
     print(allTemp +"\n")
     file.write(allTemp + "\n")
-#    allData = 
-    #count+=1
     time.sleep(sleepTime) # sleeps/waits for specified time, before taking data again
 
-    #if datetime.now().strftime('%Y-%m-%d')=='2018-08-03':
-    #    break
+    # stops the data taking at 10:00 on Aug. 6, 2018; ran this code over weekend
+    # from 20:40 on Aug. 3 to 10:00 on Aug. 6. 2018
     if datetime.now().strftime('%Y-%m-%d')=='2018-08-06' and int(datetime.now().strftime('%Y-%m-%d %H:%M:%S')[11:13])==10:
         print("YES, ending")
         break
 sock.close()
 file.close()
-
